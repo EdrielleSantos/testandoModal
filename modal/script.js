ad134619de3ef40b17ca56f3df59a8a4f9a1a8cb
Edrielle Santos <108814979+EdrielleSantos@users.noreply.github.com>
2023-05-10 00:15:01 -0300

    Add files via upload

---
+++ modal/script.js
@@ -0,0 +1,12 @@
+// Atividade
+// 
+// Crie um Modal usando HTML, CSS e JavaScript
+
+function abrirModal(){
+    document.getElementById('modal').style.display = 'flex'
+}
+
+function fecharModal(){
+    document.getElementById('modal').style.display = 'none'
+}
+console.log(fecharModal())
